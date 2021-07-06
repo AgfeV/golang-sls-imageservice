abc517a42b044f445921d8a561cff5aca39c2dc5
vincent <elysiumzero117@gmail.com>
2021-07-05 22:07:06 -0600

    goroutine requests for all s3 signed urls

--- findall/main.go
+++ findall/main.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	"strconv"
-
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"net/http"
 	"os"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"runtime"
+	"strconv"
 )
 
 var svc *dynamodb.Client
-var uploader *s3manager.Uploader
+var uploader *s3.Client
+var psClient *s3.PresignClient
 
 type Image struct {
 	ImageID   string
@@ -32,17 +32,62 @@ type Image struct {
 	SignedURL string
 }
 
+func Map(vs []Image, f func(Image) Image) []Image {
+	vsm := make([]Image, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+// S3PresignGetObjectAPI defines the interface for the PresignGetObject function.
+// We use this interface to test the function using a mocked service.
+type S3PresignGetObjectAPI interface {
+	PresignGetObject(
+		ctx context.Context,
+		params *s3.GetObjectInput,
+		optFns ...func(*s3.PresignOptions)) (*v4.PresignedHTTPRequest, error)
+}
+
 func init() {
 	// Connect to dynamoDB
-	sess := session.Must(session.NewSession())
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		panic(err)
 	}
-
 	svc = dynamodb.NewFromConfig(cfg)
-	uploader = s3manager.NewUploader(sess)
+	uploader = s3.NewFromConfig(cfg)
+	psClient = s3.NewPresignClient(uploader)
 
+}
+func GetPresignedURL(c context.Context, api S3PresignGetObjectAPI, input *s3.GetObjectInput) (*v4.PresignedHTTPRequest, error) {
+	return api.PresignGetObject(c, input)
+}
+
+// RetrieveSignedURL TODO: Sends out the request for the signed url untill I figure out how to send through channels.
+func RetrieveSignedURL(image Image, c chan Image) {
+	// Take the number of requests to fan out each
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(os.Getenv("BUCKET_NAME")),
+		Key:    aws.String(image.ImageID),
+	}
+
+	presignResp, err := GetPresignedURL(context.TODO(), psClient, input)
+	if err != nil {
+		fmt.Println("Got an error retrieving pre-signed object:")
+		fmt.Println(err)
+
+	}
+	c <- Image{
+		SignedURL: presignResp.URL,
+		UserID:    image.UserID,
+		Size:      image.Size,
+		Filename:  image.Filename,
+		ImageType: image.ImageType,
+		ImageID:   image.ImageID,
+	}
+
+	fmt.Println("The URL form goRoutine:", presignResp.URL)
 }
 func Handler(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	resp, err := svc.Query(context.TODO(), &dynamodb.QueryInput{
@@ -67,7 +112,6 @@ func Handler(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	}
 
 	// JSON string of movies to return
-	response, err := json.Marshal(images)
 	numImages := strconv.Itoa(len(images))
 	fmt.Println("Found: ", numImages, "images for the UserID: ", "123")
 
@@ -75,10 +119,24 @@ func Handler(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 		return events.APIGatewayProxyResponse{StatusCode: 404}, err
 	}
 
+	println("Number of vcpus: ", runtime.NumCPU())
+	// Number of async futures
+	c := make(chan Image)
+	// Fire off routines
+	for _, image := range images {
+		go RetrieveSignedURL(image, c)
+	}
+	// Collect the results
+	result := make([]Image, len(images))
+	for i, _ := range result {
+		result[i] = <-c
+		fmt.Println("Fan in: ", result[i].SignedURL)
+	}
+	imagesResults, err := json.Marshal(result)
 	handlerResp := events.APIGatewayProxyResponse{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            string(response),
+		Body:            string(imagesResults),
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
 			"X-MyCompany-Func-Reply": "findall-handler",
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.4.1
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.1.2
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.4.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.0
 	github.com/stretchr/testify v1.7.0 // indirect
 )
